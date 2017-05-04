a8516592295db5d3b93492788b24e7fe956f06aa
Caleb Paul <calebpaul21@gmail.com>
2017-05-04 00:38:44 -0700

    bind button and edit text add listener w intents

--- app/src/main/java/calebpaul/jrdevstory/ui/MainActivity.java
+++ app/src/main/java/calebpaul/jrdevstory/ui/MainActivity.java
@@ -1,16 +1,37 @@
 package calebpaul.jrdevstory.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import calebpaul.jrdevstory.R;
 
 public class MainActivity extends AppCompatActivity {
     public final static String TAG = MainActivity.class.getSimpleName();
 
+    private EditText nameField;
+    private Button startButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameField = (EditText) findViewById(R.id.nameEditText);
+        startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameField.getText().toString();
+                Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
     }
+
 }
