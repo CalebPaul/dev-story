0545349c134b4d602b2175acc16bc762cf197946
Caleb Paul <calebpaul21@gmail.com>
2017-05-04 10:29:03 -0700

    add code to dynamically load bg images

--- app/src/main/java/calebpaul/jrdevstory/ui/StoryActivity.java
+++ app/src/main/java/calebpaul/jrdevstory/ui/StoryActivity.java
@@ -1,24 +1,52 @@
 package calebpaul.jrdevstory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import calebpaul.jrdevstory.R;
+import calebpaul.jrdevstory.model.Page;
+import calebpaul.jrdevstory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     public final static String TAG = StoryActivity.class.getSimpleName();
+
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
+        storyImageView = (ImageView) findViewById(R.id.storyImageView);
+        storyTextView = (TextView) findViewById(R.id.storyTextView);
+        choice1Button = (Button) findViewById(R.id.choice1Button);
+        choice2Button = (Button) findViewById(R.id.choice2Button);
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         if (name == null || name.isEmpty()) {
             name = "friend";
         }
+
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImgId());
+        storyImageView.setImageDrawable(image);
     }
 }
